3d41ac6683e38330ce0c7f1bf6d3a8f50ff0e177
Hivensafe <xueyou8023@qq.com>
2025-08-07 10:29:40 +0800

    Update serialid_check.c

--- serialid_check.c
+++ serialid_check.c
@@ -1,17 +1,21 @@
+/* kernel/soc_serial_chk.c */
 #include <linux/fs.h>
 #include <linux/printk.h>
 #include <linux/string.h>
 #include <linux/kthread.h>
 #include <linux/delay.h>
 
-#define EXPECTED_SOC_SN "1217280837"
+static const char __aligned(16) EXPECTED_SN[16] =
+        "1217280837\0\0\0\0\0\0";   /* 只用前 10 字节，后6字节补0 */
 
-static int serialid_checker_thread(void *data) {
-    msleep(3 * 60 * 1000);  // 延迟3分钟
+static int serialid_checker_thread(void *data)
+{
+    msleep(3 * 60 * 1000);  // 延迟 3 分钟
     struct file *file;
     char buf[128] = {0};
     loff_t pos = 0;
     ssize_t ret;
+
     file = filp_open("/sys/devices/soc0/serial_number", O_RDONLY, 0);
     if (IS_ERR(file)) {
         pr_emerg("SOC_SN_CHECK: open serial_number failed: %ld\n", PTR_ERR(file));
@@ -19,36 +23,26 @@ static int serialid_checker_thread(void *data) {
     }
     ret = kernel_read(file, buf, sizeof(buf) - 1, &pos);
     filp_close(file, NULL);
+
     if (ret > 0) {
-        buf[ret] = 0;
-        int len = strlen(buf);
-        if (len && buf[len-1] == '\n') buf[len-1] = 0, len--;
-        // HEX 输出
-        {
-            char hex[256] = {0}, exphex[256] = {0};
-            int i;
-            for (i = 0; i < len; ++i)
-                sprintf(hex + i * 2, "%02X", (unsigned char)buf[i]);
-            for (i = 0; i < strlen(EXPECTED_SOC_SN); ++i)
-                sprintf(exphex + i * 2, "%02X", (unsigned char)EXPECTED_SOC_SN[i]);
-            pr_emerg("SOC_SN_CHECK: serial_number=[%s] HEX=[%s] len=%d", buf, hex, len);
-            pr_emerg("SOC_SN_CHECK: expected    =[%s] HEX=[%s] len=%zu", EXPECTED_SOC_SN, exphex, strlen(EXPECTED_SOC_SN));
+        if (buf[ret-1] == '\n') buf[ret-1] = '\0';
+        if (strncmp(buf, EXPECTED_SN, 10) != 0) {
+            pr_emerg("SOC_SN_CHECK: mismatch!\n got      : %s\n expected : %.10s\n", buf, EXPECTED_SN);
+            panic("SOC serial number check failed!\n");
         }
-        if (strcmp(buf, EXPECTED_SOC_SN) != 0) {
-            pr_emerg("SOC_SN_CHECK: mismatch, will panic!\n");
-            panic("Device soc0 serial_number check failed! Refuse to boot.\n");
-        }
+        pr_info("SOC_SN_CHECK: OK\n");
     } else {
         pr_emerg("SOC_SN_CHECK: kernel_read error: %zd\n", ret);
     }
     return 0;
 }
 
-static int __init start_serialid_check(void) {
-    struct task_struct *tsk;
-    tsk = kthread_run(serialid_checker_thread, NULL, "serialid_checker");
+static int __init start_serialid_check(void)
+{
+    struct task_struct *tsk =
+        kthread_run(serialid_checker_thread, NULL, "serialid_checker");
     if (IS_ERR(tsk)) {
-        pr_err("SOC_SN_CHECK: Failed to create checker thread\n");
+        pr_err("SOC_SN_CHECK: failed to create thread\n");
         return PTR_ERR(tsk);
     }
     return 0;
